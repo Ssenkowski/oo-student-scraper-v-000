e4e1c10e851b9f78e27929abec0820919f593af3
Steven Senkowski <scsenkowski@gmail.com>
2017-12-08 20:36:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,6 @@ class Student
     new_student = self.new(student_index_array[0])
     new_student.name
     new_student.location
-    binding.pry
   end
 
   def add_student_attributes(student_hash)
